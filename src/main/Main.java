003c4406c4b00133fa0293781fdea564bb215f5f
jcfern87 <136532734+jcfern87@users.noreply.github.com>
2024-01-17 17:45:55 +0000

    ordenar por contagem

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,34 +1,13 @@
+import ordenadores.Ordenador;
+import ordenadores.OrdenarPorContagem;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        int[] array = { 8, 7, 6, 5, 4, 3, 2, 1, 0 };
-        int i = partition(array, 0, array.length - 1);
-        System.out.println("valor de i " + i);
-        for (int j = 0; j < array.length; j++) {
-            System.out.println(array[j]);
+        int[] array = {5, 2, 7, 4, -2, 2};
+        Ordenador o = new OrdenarPorContagem();
+        o.ordene(array);
+        for (int i : array) {
+            System.out.println(i);
         }
-    }
-
-
-    public static int partition(int[] values, int left, int right) {
-        
-        int pivot = values[left];
-        int i = left;
-
-        for (int j = left + 1; j <= right; j++) {
-            if (values[j] <= pivot) {
-                i+=1;
-                int temp = values[i];
-                values[i] = values[j];
-                values[j] = temp;
-            }
-        }
-
-        // troca pivot (values[left]) com i.
-        int temp = values[i];
-        values[i] = values[left];
-        values[left] = temp;
-        
-        return i; 
     }
 }
--- src/main/ordenadores/OrdenarPorContagem.java
+++ src/main/ordenadores/OrdenarPorContagem.java
@@ -3,6 +3,39 @@ public class OrdenarPorContagem implements Ordenador{
 
     @Override
     public int[] ordene(int[] array) {
+        int min = array[0];
+        int max = array[0];
+        for (int i : array) {
+            if (i < min) {
+                min = i;
+            }
+            if (i > max) {
+                max = i;
+            }
+        }
+        int size = 0;
+        if (min < 0) {
+            size = max - min + 1;
+        }
+        else {
+            size = max + 1;
+        }
+        int[] order = new int[size];
+        int index = 0;
+        for (int i = 0; i < array.length; i++) {
+            index = array[i] - min;
+            order[index]++;        
+        }
+        int j = 0;
+        for (int i = 0; i < order.length; i++) {
+            if(order[i] > 0){
+                while(order[i] > 0) {
+                    array[j] = i + min;
+                    order[i]--;
+                    j++;
+                }
+            }
+        }
         return array;
     }
 
